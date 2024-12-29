6cae6d99062fe05ee3531bd073b984d6d7dba170
ahujamonica <ahujamonica9321@gmail.com>
2024-12-29 06:47:05 +0000

    variables declaration

---
+++ 02_constVar/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "ahujamonica9321@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+// accountId = 2 -- not allowed
+accountEmail = "ma@ma.com"
+accountPassword = "23234"
+accountCity = "Gujarat"
+let accountState;
+
+// console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState])
+
+/*
+Prefer not to use var because of its scope issue 
+*/
