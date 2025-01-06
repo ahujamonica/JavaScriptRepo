299cfc899bf2e568c3fe0e349b4642ebebf97a66
Monica Ahuja <ahujamonica9321@gmail.com>
2025-01-06 12:26:20 +0000

    intro to strings

---
+++ 01_basics/02_strings.js
@@ -0,0 +1,8 @@
+const name = "Monica"
+const repoCount = 50
+
+console.log(name + repoCount); // this one is outdated 
+
+// *****string interpollation****
+console.log(`hello, my name is ${name} and my rep count is ${repoCount}`);
+
